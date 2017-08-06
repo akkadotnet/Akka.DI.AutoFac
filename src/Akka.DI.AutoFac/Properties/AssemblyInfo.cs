a0ed5db639afbfa8d3acd090687afefd3977c101
ravengerUA <yurisikorsky@gmail.com>
2017-08-06 19:51:31 +0300

    Added support for NetStandard

--- src/Akka.DI.AutoFac/Properties/AssemblyInfo.cs
+++ src/Akka.DI.AutoFac/Properties/AssemblyInfo.cs
@@ -8,15 +8,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Akka.DI.AutoFac")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Akka.DI.AutoFac")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
